92868e4dc74eba898a42e22bf81d16620893ae39
Yutong Zhu <y25zhu@uwaterloo.ca>
2024-10-14 13:34:25 -0400

    Fix web driver quit early

--- q2.py
+++ q2.py
@@ -1,36 +1,49 @@
-from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+from selenium import webdriver
 from bs4 import BeautifulSoup
 import json
-driver = webdriver.Chrome()
 
+driver = webdriver.Chrome()
 # Open the webpage
 URL = "https://www.microfocus.com/en-us/products?trial=true"
 driver.get(URL)
 
-driver.implicitly_wait(20)
-html = driver.page_source
-driver.quit()
+try:
+    WebDriverWait(driver, 30).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "products-grid"))
+    )
+
+    html = driver.page_source
+
+finally:
+    driver.quit()
 
 soup = BeautifulSoup(html, 'html.parser')
 
 product_grid = soup.find('div', class_='products-grid')
-grids = soup.find_all('div', class_='uk-card uk-card-hover uk-card-default uk-overflow-hidden uk-margin-large-bottom uk-position-relative')
+grids = soup.find_all(
+    'div', class_='uk-card uk-card-hover uk-card-default uk-overflow-hidden uk-margin-large-bottom uk-position-relative')
+
 res = []
+
 for grid in grids:
     product_name = grid.find('div', class_='title').text.strip()
     starting_letter = product_name[0].upper()
 
-    links = grid.find('div', class_ = 'cta-buttons uk-flex uk-flex-middle').find_all('a') # Assume all cards must have this field
+    # Assume all cards must have this field
+    links = grid.find(
+        'div', class_='cta-buttons uk-flex uk-flex-middle').find_all('a')
     demo_url = ""
     free_trial_url = ""
     for button in links:
-        if button.text == "Get free trial": # Assume there are only two types of texts
+        if button.text == "Get free trial":  # Assume there are only two types of texts
             free_trial_url = button.get("href")
         else:
             demo_url = button.get("href")
 
-    footer = grid.find('div', class_ = "footer")
+    footer = grid.find('div', class_="footer")
     community_url = ""
     support_url = ""
 
@@ -44,11 +57,11 @@ for grid in grids:
             if text == 'Community':
                 community_url = link.get("href")
             elif text == 'Support':
-               support_url = link.get("href")
-    
+                support_url = link.get("href")
+
     res.append({
         'name': product_name,
-        'start_letter':starting_letter,
+        'start_letter': starting_letter,
         'demo_url': demo_url,
         'free_trial_url': free_trial_url,
         'community_url': community_url,
